00eab8658a7d703d548d69b274b70872dc85a597
fnowakow <f.nowakowski2000@gmail.com>
2023-10-29 12:54:26 +0100

    zadanie 4

--- src/MyAgent.java
+++ src/MyAgent.java
@@ -6,10 +6,15 @@ import jade.lang.acl.*;
 import jade.domain.*;
 import jade.domain.FIPAAgentManagement.*;
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class MyAgent extends Agent {
+
+	public record Request(String content, String ontology, String response) {}
+	public Map<String, Request> requests = new HashMap<>();
+
 	protected void setup () {
 		addBehaviour(new MyCyclicBehaviour(this));
 		//doDelete();
@@ -47,6 +52,10 @@ class MyCyclicBehaviour extends CyclicBehaviour {
 		} else {
 			String ontology = message.getOntology();
 			String content = message.getContent();
+			String msgId = message.getInReplyTo();
+
+			System.out.println("msgId : " + msgId);
+
 			int performative = message.getPerformative();
 			if (performative == ACLMessage.REQUEST)
 			{
@@ -61,13 +70,21 @@ class MyCyclicBehaviour extends CyclicBehaviour {
 					if (result.length == 0) myAgent.displayResponse("No service has been found ...");
 					else
 					{
+						msgId = generateId();
+
 						String foundAgent = result[0].getName().getLocalName();
-						myAgent.displayResponse("Agent " + foundAgent + " is a service provider. Sending message to " + foundAgent);
+						String info = "Agent %s is service provider, sending message. [%s]"
+								.formatted(foundAgent, msgId);
+						myAgent.displayResponse(info);
 						ACLMessage forward = new ACLMessage(ACLMessage.REQUEST);
 						forward.addReceiver(new AID(foundAgent, AID.ISLOCALNAME));
 						forward.setContent(content);
 						forward.setOntology(ontology);
+						forward.setReplyWith(msgId);
 						myAgent.send(forward);
+
+						MyAgent.Request request = new MyAgent.Request(content, ontology, null);
+						this.myAgent.requests.put(msgId, request);
 					}
 				}
 				catch (FIPAException ex)
@@ -78,8 +95,18 @@ class MyCyclicBehaviour extends CyclicBehaviour {
 			}
 			else
 			{	//when it is an answer
+				MyAgent.Request request = myAgent.requests.get(msgId);
+				MyAgent.Request withResponse = new MyAgent.Request(
+						request.content(), request.ontology(), content);
+				myAgent.requests.put(msgId, withResponse);
+
+				myAgent.displayResponse("%s received".formatted(msgId));
 				myAgent.displayHtmlResponse(content);
 			}
 		}
+	}
+
+	protected String generateId() {
+		return String.valueOf( new Random().nextInt() );
 	}
 }
